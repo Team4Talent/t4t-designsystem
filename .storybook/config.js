93ce1cce39d67c8c48d6f264234bb617b2ea175a
Benjamin Todts <benjamin.todts@t4t.rocks>
2020-01-16 17:05:55 +0100

    Add react-icons

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,4 +2,8 @@ import React from 'react';
 import { addDecorator } from '@storybook/react';
 
 // center the components in the storybook view
-addDecorator(storyFn => <div style={{ textAlign: 'center', fontFamily: 'Nunito Sans' }}>{storyFn()}</div>);
+addDecorator(storyFn => (
+  <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', fontFamily: 'Nunito Sans' }}>
+    {storyFn()}
+  </div>
+));
