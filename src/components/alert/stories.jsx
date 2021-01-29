2868f301fa2251d62b7773486174667af0088eba
Benjamin Todts <benjamin.todts@t4t.rocks>
2021-01-29 21:09:27 +0100

    Add code docs to Pill and Alert

--- src/components/alert/stories.jsx
+++ src/components/alert/stories.jsx
@@ -6,12 +6,33 @@ export default {
     component: Alert
 };
 
+var createDocsObj = type => {
+    return { source: {
+        code: `<Alert type=\'${type}\'>This is an alert notification.</Alert>;`
+    }}
+}
+
 export const Success =  () => <Alert type='success'>This is an alert notification.</Alert>;
+Success.parameters = {
+    docs: createDocsObj('success')
+}
 
 export const Error = () => <Alert type='error'>This is an alert notification.</Alert>;
+Error.parameters = {
+    docs: createDocsObj('error')
+}
 
 export const Warning = () => <Alert type='warning'>This is an alert notification.</Alert>;
+Warning.parameters = {
+    docs: createDocsObj('warning')
+}
 
 export const Info = () => <Alert>This is an alert notification.</Alert>;
+Info.parameters = {
+    docs: createDocsObj('info')
+}
 
 export const Neutral = () => <Alert type='neutral'>This is an alert notification.</Alert>;
+Neutral.parameters = {
+    docs: createDocsObj('neutral')
+}
--- src/components/pill/stories.jsx
+++ src/components/pill/stories.jsx
@@ -6,12 +6,33 @@ export default {
     component: Pill
 };
 
+var createDocsObj = type => {
+    return { source: {
+        code: `<Pill type=\'${type}\'>${type}</Pill>;`
+    }}
+}
+
 export const Primary = () => <Pill>primary</Pill>;
+Primary.parameters = {
+    docs: createDocsObj('primary')
+}
 
 export const Success = () => <Pill type='success'>success</Pill>;
+Success.parameters = {
+    docs: createDocsObj('success')
+}
 
 export const Error = () => <Pill type='error'>error</Pill>;
+Error.parameters = {
+    docs: createDocsObj('error')
+}
 
 export const Warning = () => <Pill type='warning'>warning</Pill>;
+Warning.parameters = {
+    docs: createDocsObj('warning')
+}
 
 export const Neutral = () => <Pill type='neutral'>neutral</Pill>;
+Neutral.parameters = {
+    docs: createDocsObj('neutral')
+}
